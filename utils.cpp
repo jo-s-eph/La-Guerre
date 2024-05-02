81e7eed630501e77654cb688c602ea3f45a0ac0a
developpeur_B <maouchejoseph@gmail.com>
2024-05-02 13:41:59 +0200

    enhanced prints functions and coordinates asking for all purposes of the game

--- utils.cpp
+++ utils.cpp
@@ -1,5 +1,29 @@
 #include "utils.h"
 
+
 int manhattanDistance(int x1, int y1, int x2, int y2) {
     return abs(x1 - x2) + abs(y1 - y2);
+}
+
+std::pair<int,int> askCoord(std::string question)
+{
+    int userx, usery;
+    while (true) {
+        std::cout << question << "(x y) : ";
+        std::cin >> userx >> usery;
+        if (userx < 1 || userx > TAILLE || usery < 1 || usery > TAILLE)
+            printErr("Coordonnées hors du plateau.");
+        else
+            return std::make_pair(userx, usery);
+    }
+}
+
+void printErr(std::string msg)
+{
+    std::cerr << RED << " ✕ " << RESET << ' ' << msg << std::endl;
+}
+
+void printValid(std::string msg)
+{
+    std::cerr << GREEN2 << " ✓ " << RESET << ' ' << msg << std::endl;
 }
--- utils.h
+++ utils.h
@@ -2,9 +2,10 @@
 #define UTILS_H
 
 #include <cmath>
+#include <iostream>
 
 #define TAILLE 20
-#define OR_DEPART 5
+#define OR_DEPART 200
 
 #define RESET   "\033[0m"
 #define RED     "\033[0;101m"
@@ -12,9 +13,14 @@
 #define GREY    "\033[90m"
 #define YELLOW  "\033[1m\033[33m"
 #define GREEN   "\033[0m\033[32m"
+#define GREEN2   "\033[0;102m"
 #define WHITE  "\033[1m\033[37m"
 
 
 int manhattanDistance(int x1, int y1, int x2, int y2);
+
+std::pair<int, int> askCoord(std::string question);
+void printErr(std::string msg);
+void printValid(std::string msg);
 
 #endif // UTILS_H
